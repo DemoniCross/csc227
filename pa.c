1240d5d15ae782d80a9fea65e796c2bad63ff4c0
DemoniCross <ammar00h@gmail.com>
2021-06-12 12:37:12 +0300

    last

--- pa.c
+++ pa.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
 int gVar = 100;
 
 int main() {
-  pid_t firstChild = fork(), secondChild;
+  pid_t firstChild, secondChild;
+  firstChild = fork();
   if(firstChild == 0) {
-    gVar++;
-    printf("%d\n", gVar);
+    printf("%d", ++gVar);
   }
   else {
     secondChild = fork();
-    if(secondChild == 0) {
-      gVar--;
-      printf("%d\n", gVar);
+    if(secondChild == 0){
+      printf("%d", --gVar);
+    } else {
+      sleep(1);
+      printf("%d", gVar);
     }
-    else
-      printf("%d\n", gVar);
   }
   return 0;
 }
